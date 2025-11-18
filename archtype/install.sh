de21c9519db935e559679526972251f49b07f1d2
viztini <leon.stonehouseburguillo@education.nsw.gov.au>
2025-11-18 12:28:23 +1100

    Delete archtype directory

--- archtype/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SUDO=''
-if [ "$EUID" -ne 0 ]; then
-  SUDO='sudo'
-fi
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-$SUDO ln -sf "$SCRIPT_DIR/main.py" /usr/local/bin/archtype
-echo "archtype installed successfully!"
-echo "You can now run the game by typing: archtype"
